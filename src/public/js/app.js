7b38e4a625299ebd20ec66c59c15ceddbad4b81e
Y00J00 <syeunnn@gmail.com>
2023-05-31 23:50:01 +0900

    2023-05-31

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,32 @@
 // 프론트와 벡엔드 연결
 const socket = new WebSocket(`ws://${window.location.host}`); 
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 
 
 // ws에는 listen할 특정한 event명이 있고
 // ws에서도 추가적인 정보를 받을 function이 존재
+socket.addEventListener("open", handleOpen);
 
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText  = message.data;
+    messageList.appen(li);
+});
+
+function handleNickSubmint(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = '';
+}
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
 //server.js의 socket은 연결된 웹 브라우저
 // app.js의 socket은 서버로의 연결
--- src/server.js
+++ src/server.js
@@ -16,22 +16,40 @@ app.use("/public", express.static(__dirname + "/src/public")); // public url 생
 app.get("/", (_, res) => res.render("home")); //home.pug를 render해주는 route handler
 app.get("/*", (_, res) => res.redirect("/"));
 
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer)
 const handleListen = () => console.log(`listening on http://localhost:3000`); 
 //const handleListen = () => console.log(`listening on ws://localhost:3000`); 도 가능
 
-const server = http.createServer(app); // 서버 만들기
-const wss = new WebSocketServer({ server }); // http서버 위에 webSocket서버를 만듬 -> 이렇게 하면 같은 서버에서 http , ws 둘다 작동시킬수 있다.
-//포트 3000에 두 개의 서버 
+// const server = http.createServer(app); // 서버 만들기
+// const wss = new WebSocketServer({ server }); // http서버 위에 webSocket서버를 만듬 -> 이렇게 하면 같은 서버에서 http , ws 둘다 작동시킬수 있다.
+// //포트 3000에 두 개의 서버 
 
-function handleConnection(socket) { // 콜백으로 소켓 받았음.
-    console.log(socket);
-    // server.js의 socket은 연결된 웹 브라우저
-    // app.js의 socket은 서버로의 연결
-}
-wss.on("connection", handleConnection) 
+// function handleConnection(socket) { // 콜백으로 소켓 받았음.
+//     console.log(socket);
+//     // server.js의 socket은 연결된 웹 브라우저
+//     // app.js의 socket은 서버로의 연결
+// }
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     console.log('connect browser');
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (message) => { // 메세지의 타입을 만들어서 채팅과 닉네임을 구분
+//         const parsed = JSON.parse(message);
+//         switch(parsed.type) {
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+//             case "nicknmae" : 
+//                 socket["nickname"] = message.payload;
+//         }
+//     })
+// }) 
 // on 메소드에서는 이벤트가 발동하는 것을 기다림 + 벡엔드에 연결된 사람의 정보를 제공 (소켓에서)
 //connection 이벤트가 이루어지면 handleConnection을 발생시킴. 이때 callback으로 socket을 받음. 
 // socket => 나(서버)와 브러우저 사이의 연결. 연결된 브러우저와의 컨택라인. 소켓을 이용하여 메세지를 주고 받을수 있음
 
 server.listen(3000, handleListen);
 // dev는 nodemon을 호출하고 nodemon은 호출되면 nodemon.json안의 코드를 실행함 -> npm run dev
+
+
